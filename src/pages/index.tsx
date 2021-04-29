f009d225bae2eea29a192bceb37960b36b1bf256
Julio Machado <juliocarlos00@hotmail.com>
2021-04-29 18:49:45 -0300

    add/ logo + banner on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import { Image } from "@chakra-ui/image";
+import { Box, Flex } from "@chakra-ui/layout";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <Flex h="100vh" direction="column">
+      <Flex as="header" justify="center" align="center" my="4">
+        <Box>
+          <Image src="./images/logo.png" alt="worldtrip logo" />
+        </Box>
+      </Flex>
+      <Flex as="header" justify="center" align="center" mb="20">
+        <Box>
+          <Image src="./images/banner.png" alt="worldtrip logo" maxH={368.21}/>
+        </Box>
+      </Flex>
+    </Flex >
   )
 }
