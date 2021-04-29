467175aa1c11cb8f75488a3548255ef4843a1cf7
Julio Machado <juliocarlos00@hotmail.com>
2021-04-29 18:19:59 -0300

    add/ chakra-ui

---
+++ styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+    colors: {
+        gray: {
+            "700": "#47585B",
+            "400": "#999999",
+            "50": "#FFFFFF40",
+        },
+        yellow: {
+            "900": "#FFBA08",
+            "850": "#FFBA0880"
+        }
+    },
+    fonts: {
+        heading: 'Poppins',
+        body: 'Poppins'
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'gray.50',
+                color: 'gray.700'
+            }
+        }
+    }
+})
